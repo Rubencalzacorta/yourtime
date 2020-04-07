09979a8c2f4db318496292853a19d9dfe6dc5238
Ruben Calzacorta <calzacorta.ruben@gmail.com>
2020-04-07 22:42:51 +0200

    Deploying

---
+++ server/public/precache-manifest.9e8445dec66ca9ed97918791a2ad7916.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd5dca306c9b3f7298763fdb6b1fa53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1dd70e22977a871513c",
+    "url": "/static/css/main.72dc1fea.chunk.css"
+  },
+  {
+    "revision": "473d6cc0acba4b6504a4",
+    "url": "/static/js/2.56fbcf9e.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/static/js/2.56fbcf9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1dd70e22977a871513c",
+    "url": "/static/js/main.bd64f107.chunk.js"
+  },
+  {
+    "revision": "cac28054c7838a09a23c",
+    "url": "/static/js/runtime-main.b496b7fa.js"
+  },
+  {
+    "revision": "a391b7070f973f69a8f2ae935d152e67",
+    "url": "/static/media/d511ce029edd7821931446e4bd7266bd.a391b707.jpg"
+  }
+]);
