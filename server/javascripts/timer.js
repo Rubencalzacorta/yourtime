6e28d307fe848b4c03c9a8a5a9390a390a271d78
Ruben Calzacorta <calzacorta.ruben@gmail.com>
2020-04-13 11:12:20 +0200

    Deploying

--- server/javascripts/timer.js
+++ server/javascripts/timer.js
@@ -2,20 +2,20 @@ const express = require('express');
 const Todo = require('../models/Todo')
 const router = express.Router();
 
-//every 2 seconds, this interval checks for todos that are being done and adds 2 seconds. 
+//every 1 seconds, this interval checks for todos that are being done and adds 2 seconds. 
 
 setInterval(() => {
 
     Todo.find({ status: "Doing" })
         .then(allDoing => {
             allDoing.forEach((elm) => {
-                Todo.findByIdAndUpdate(elm._id, { $inc: { time: 2 } }, { new: true })
+                Todo.findByIdAndUpdate(elm._id, { $inc: { time: 1 } }, { new: true })
                     .catch(err => console.log("RC error adding to time in the back"))
             })
         })
         .catch(err => console.log("RC err looking for the doing", err))
 
-}, 2000)
+}, 1000)
 
 
 
